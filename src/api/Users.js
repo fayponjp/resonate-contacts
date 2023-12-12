00122ed0a3bb0f1a8392bcb88880f92f992aa1a6
Jon Paulo Faypon <jpfaypon@gmail.com>
2023-12-12 17:43:38 +0800

    Set up initial application components

---
+++ src/api/Users.js
@@ -0,0 +1,15 @@
+export async function getUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error retrieving data: ", error.message);
+    }
+}
